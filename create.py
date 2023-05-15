f8906bc7402a6e51a7a12d1550dc682a333e65bf
dankeyy <dankeyy@protonmail.com>
2023-05-15 23:26:56 +0300

    logic was fine to begin with, py bug

--- create.py
+++ create.py
@@ -1,36 +1,24 @@
 def f():
-    [];[][1]
+    [][1]
 
 def g():
-    1, f()
-
+    f()
 
 try:
-    1, g()
+    g()
 except Exception as e:
     tb = e.__traceback__
-
-
-    # there's this small annoyance that the last -last-instruction- is relevant only in the context of the first frame
-    # so we need to preserve a stack for the traces and walk backwards with the lasti-s
-    # feels bad to be needing to allocate here, c'est la vie
-    traces = []
-    last_instructions = []
-    while tb:
-        traces.append(tb)
-        last_instructions.append(tb.tb_lasti)
-        tb = tb.tb_next
-    frames_count = len(last_instructions)
-
     locations = []
     positions = None
-    for ti, t in enumerate(traces):
+
+    while tb:
         # i-th positions tuple means i-th instruction
         # as explained in https://docs.python.org/3/reference/datamodel.html#codeobject.co_positions
-        for i, positions in enumerate(t.tb_frame.f_code.co_positions()):
-            if i == last_instructions[frames_count - ti - 1]:
+        for i, positions in enumerate(tb.tb_frame.f_code.co_positions()):
+            if i == tb.tb_lasti:
                 break
         assert positions is not None # should _never_ be the case
         locations.extend(positions)
+        tb = tb.tb_next
 
     print(locations)
